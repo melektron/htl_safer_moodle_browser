a895a7339c771e60eaf5f47a29f520a08f8c25a6
melektron <matteo@elektron.work>
2023-05-22 12:29:25 +0200

    added better question system

---
+++ classes/ui/questions.py
@@ -0,0 +1,41 @@
+"""
+ELEKTRON © 2023
+Written by melektron
+www.elektron.work
+22.05.23, 11:23
+
+
+"""
+
+
+from ._question import Question
+import random as r
+import tkinter as tk
+import customtkinter as ctk
+
+class QuestionMark(Question):
+    def __init__(self, master):
+        super().__init__(master, "What is your FSST Mark?")
+
+        mark_value = tk.IntVar()
+        mark_value.set(5)
+        create_random_mark = lambda: mark_value.set(r.randint(1, 5))
+        self.add(ctk.CTkRadioButton(self, variable=mark_value, text=1, value=1, command=create_random_mark))
+        self.add(ctk.CTkRadioButton(self, variable=mark_value, text=2, value=2, command=create_random_mark))
+        self.add(ctk.CTkRadioButton(self, variable=mark_value, text=3, value=3, command=create_random_mark))
+        self.add(ctk.CTkRadioButton(self, variable=mark_value, text=4, value=4, command=create_random_mark))
+        self.add(ctk.CTkRadioButton(self, variable=mark_value, text=5, value=5, command=create_random_mark))
+
+
+class Question(Question):
+    def __init__(self, master):
+        super().__init__(master, "What is your FSST Mark?")
+
+        mark_value = tk.IntVar()
+        mark_value.set(5)
+        create_random_mark = lambda: mark_value.set(r.randint(1, 5))
+        self.add(ctk.CTkRadioButton(self, variable=mark_value, text=1, value=1, command=create_random_mark))
+        self.add(ctk.CTkRadioButton(self, variable=mark_value, text=2, value=2, command=create_random_mark))
+        self.add(ctk.CTkRadioButton(self, variable=mark_value, text=3, value=3, command=create_random_mark))
+        self.add(ctk.CTkRadioButton(self, variable=mark_value, text=4, value=4, command=create_random_mark))
+        self.add(ctk.CTkRadioButton(self, variable=mark_value, text=5, value=5, command=create_random_mark))
--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import tkinter as tk
 import tkinter.messagebox as msg
 import customtkinter as ctk
 import random as r
+import classes.ui.questions as q
 from classes.ui import Question, FONT_HEADER
 
 main_window: ctk.CTk
 real_close_flag = False
+
+questions: list[Question] = None
+current_question: Question = None
+question_index: int = 0
+
+
 
 def real_close(e=None):
     main_window.destroy()
@@ -15,17 +22,39 @@ def normal_close():
     msg.showinfo("!!!!", "fu, you won't get rid of me")
 
 
+def show_question(new_question: Question):
+    global current_question
+    if current_question is not None:
+        current_question.grid_forget()
+    current_question = new_question
+    current_question.grid(row=1, column=0, columnspan=2, sticky="nsew", padx=10)
 
+def next_question():
+    global question_index
+    question_index += 1
+    if question_index > len(questions) - 1:
+        question_index = len(questions) - 1
+        return# show check button
+    show_question(questions[question_index])
 
-
+def last_question():
+    global question_index
+    question_index -= 1
+    if question_index < 0:
+        question_index = 0
+        return# disable back button
+    show_question(questions[question_index])
 
 
 def main():
-    global main_window
+    global main_window, questions
 
     main_window = ctk.CTk()
+    main_window.geometry("600x250")
     main_window.title(".")
     main_window.grid_columnconfigure(0, weight=1)
+    main_window.grid_columnconfigure(1, weight=1)
+    main_window.grid_rowconfigure(2, weight=1)
 
     # pressing o will actually close the window
     main_window.bind("o", real_close)
@@ -33,29 +62,26 @@ def main():
     # closing the window the normal way will do wired stuff
     main_window.protocol("WM_DELETE_WINDOW", normal_close)
 
-
     # Title
     lb_exam_title = ctk.CTkLabel(main_window, text="Solve this simple exam", font=FONT_HEADER)
-    lb_exam_title.grid(row=0, column=0)
+    lb_exam_title.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
+    # Question area
+    questions = [
+        q.QuestionMark(main_window),
+        q.QuestionMark(main_window),
+        q.QuestionMark(main_window),
+    ]
 
-    # first question
-    question = Question(main_window, "1. What is our FSST mark?")
-    question.grid(row=1, column=0, sticky="nsew", padx=10)
-    mark_value = tk.IntVar()
-    mark_value.set(5)
-    create_random_mark = lambda: mark_value.set(r.randint(1, 5))
-    question.add(ctk.CTkRadioButton(question, variable=mark_value, text=1, value=1, command=create_random_mark))
-    question.add(ctk.CTkRadioButton(question, variable=mark_value, text=2, value=2, command=create_random_mark))
-    question.add(ctk.CTkRadioButton(question, variable=mark_value, text=3, value=3, command=create_random_mark))
-    question.add(ctk.CTkRadioButton(question, variable=mark_value, text=4, value=4, command=create_random_mark))
-    question.add(ctk.CTkRadioButton(question, variable=mark_value, text=5, value=5, command=create_random_mark))
+    show_question(questions[question_index])
 
+    # back button
+    bt_back = ctk.CTkButton(main_window, text="Back", command=last_question)
+    bt_back.grid(row=2, column=0, pady=10, sticky="s")
 
-
-    # cursed radio buttons
-    lb_exam_title = ctk.CTkLabel(main_window, text="Solve this simple exam")
-    lb_exam_title.grid(row=0, column=0, columnspan=3)
+    # next button
+    bt_next = ctk.CTkButton(main_window, text="Next Question", command=next_question)
+    bt_next.grid(row=2, column=1, pady=10, sticky="s")
 
 
 
