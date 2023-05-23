633952fce7edcae646d3819be591c266573bef34
melektron <matteo@elektron.work>
2023-05-23 10:40:55 +0200

    added new questions

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ def main():
     global main_window
     main_window = AppWindow()
     main_window.set_questions(
+        q.QuestionSmartPerson(main_window.question_area),
         q.QuestionMark(main_window.question_area),
         q.QuestionStrongPassword(main_window.question_area),
+        q.QuestionGoingToSchool(main_window.question_area)
     )
     main_window.mainloop()
 
